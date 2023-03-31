45407837e467fe172fc44d9fc0620f19a4dde75c
Matthias Eckinger <Matthias.Eckinger@binderholz.com>
2023-03-31 10:52:10 +0200

    Befüllen der TextBoxen

---
+++ ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tennisplatzreservierung.Models;
+
+namespace Tenniscourtreservation.ViewModels
+{
+    internal class MakeReservationViewModel : ViewModelBase
+    {
+        #region properties
+
+        private string _playername;
+        public string Playername
+        {
+            get
+            {
+                return _playername;
+            }
+            set
+            {
+                _playername = value;
+                OnPropertyChanged(nameof(Playername));
+            }
+
+        }
+
+        private int _courtNumber;
+        public int CourtNumber
+        {
+            get
+            {
+                return _courtNumber;
+            }
+            set
+            {
+                _courtNumber = value;
+                OnPropertyChanged(nameof(CourtNumber));
+            }
+
+        }
+
+        private int _courtSide;
+        public int CourtSide
+        {
+            get
+            {
+                return _courtSide;
+            }
+            set
+            {
+                _courtSide = value;
+                OnPropertyChanged(nameof(CourtSide));
+            }
+
+        }
+
+        private DateTime _startTime;
+        public DateTime StartTime
+        {
+            get
+            {
+                return _startTime;
+            }
+            set
+            {
+                _startTime = value;
+                OnPropertyChanged(nameof(StartTime));
+            }
+
+        }
+
+        private DateTime _endTime;
+        public DateTime EndTime
+        {
+            get
+            {
+                return _endTime;
+            }
+            set
+            {
+                _endTime = value;
+                OnPropertyChanged(nameof(EndTime));
+            }
+
+        }
+
+        #endregion
+
+        #region commands
+
+        public ICommand SumitCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        #endregion
+
+        public MakeReservationViewModel()
+        {
+            
+        }
+
+    }
+}
---
+++ ViewModels/ReservationViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tennisplatzreservierung.Models;
+
+namespace Tenniscourtreservation.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        #region fields
+
+        private readonly Reservation _reservation;
+
+        #endregion
+
+        #region properties
+
+        public string CourtID => _reservation.CourtID.ToString();
+        public string Playername => _reservation.Playername;
+        public DateTime StartTime => _reservation.StartTime;
+        public DateTime EndTime => _reservation.EndTime;
+        public TimeSpan Length => EndTime.Subtract(StartTime);
+
+        #endregion
+
+        public ReservationViewModel(Reservation reservation)
+        {
+            _reservation = reservation;
+        }
+
+    }
+}
